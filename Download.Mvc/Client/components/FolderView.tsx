92e497029df22178e59ca4fb6ebdb5bd8764e97f
Scott Kirkland <srkirkland@gmail.com>
2017-07-06 16:15:00 -0700

    starting point for react app

---
+++ Download.Mvc/Client/components/FolderView.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+
+export class FolderView extends React.Component<{}, {}> {
+    render() {
+        return <div>
+            <h1>Viewing folder!</h1>
+        </div>;
+    }
+}
