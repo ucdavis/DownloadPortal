e3b6b9624be4654b4b8a9ad121ccde32067d9103
Scott Kirkland <srkirkland@gmail.com>
2017-07-06 17:00:10 -0700

    do a fetch for api info

--- Download.Mvc/Client/components/FolderView.tsx
+++ Download.Mvc/Client/components/FolderView.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { match } from 'react-router-dom';
+import 'isomorphic-fetch';
 
 interface IRouteParams {
     id: string
@@ -10,13 +11,26 @@ export interface IProps {
 }
 
 export class FolderView extends React.Component<IProps, any> {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: {
+                total_count: 0,
+                entries: []
+            }
+        };
+    }
     componentDidMount = () => {
-        alert('mounted');
+        // go grab the folder info we are looking at
+        fetch('/api/folder/' + this.props.match.params.id, { credentials: 'same-origin' })
+            .then(response => response.json())
+            .then(data => this.setState({ data }));
     }
     render() {
-
         return <div>
             <h1>Viewing folder {this.props.match.params.id}!</h1>
+            <p>There are {this.state.data.total_count} things in here</p>
         </div>;
     }
 }
