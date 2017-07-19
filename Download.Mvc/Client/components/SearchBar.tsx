b693801af5e6c9d24717c3d4a50676a4cbf545e5
Laura Holstege <laholstege@ucdavis.edu>
2017-07-19 14:12:44 -0700

    updated SearchBox form

--- Download.Mvc/Client/components/SearchBar.tsx
+++ Download.Mvc/Client/components/SearchBar.tsx
@@ -13,15 +13,12 @@ export class SearchBar extends React.Component<any, any>{
         this.setState({ query: e.target.value });
     }
 
-    handleSubmit = (e) => {
-        window.location.href = "../../search/" + encodeURIComponent(this.state.query);
-    }
     render() {
         return (
-            <form>
+            <form action={`../../search/${encodeURIComponent(this.state.query)}`} method="get">
                 <input type="text" value={this.state.query} placeholder="Search . . ." onChange={this.handleChange} />
-                <Link to={`../../search/${this.state.query}`}>
-                    <button type="submit" value="Submit">
+                <Link to={`../../search/${encodeURIComponent(this.state.query)}`}>
+                    <button>
                         <i className="fa fa-search"></i>
                     </button>
                 </Link>
